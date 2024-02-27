741448788cf316580199cf7d2119ae626f5803d3
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-02-27 14:41:35 -0300

    feat: Criação da seção de sobre e habilidades.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { Coins, Database, File, Globe, Headphones, LineChart, Smartphone } from "lucide-react";
+
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <div className="bg-stone-800 w-screen">
         <div className="mx-auto max-w-7xl">
           <nav className="flex items-center justify-between text-white h-24">
@@ -26,7 +28,7 @@ export default function Home() {
               <h4 className="font-light text-white text-2xl">Olá!</h4>
               <h1 className="text-white font-semibold text-5xl">Eu sou desenvolvedor...</h1>
               <p className="font-light text-white text-2xl">Especializado em Sites Responsivos</p>
-              <a href="#" className="px-4 py-2 color-white bg-emerald-700 text-white w-max text-lg">Fale comigo</a>
+              <a href="#" className="px-4 py-2 color-white rounded-sm bg-green-500 text-black w-max text-lg">Fale comigo</a>
             </div>
 
             <div className="absolute bottom-10 left-0 text-white flex gap-6">
@@ -46,18 +48,71 @@ export default function Home() {
               </div>
             </div>
           </section>
-
-          <section className="flex flex-row bg-black w-full">
-            <div className="text-white 1/3">
-              ...
-            </div>
-            
-            <div className="text-white 2/3 bg-red-500">
-              ...
-            </div>
-
-          </section>
         </div>
+
+        <section className="w-full bg-neutral-950 h-max">
+          <div className="mx-auto max-w-7xl flex flex-row py-12">
+            <div className="text-white w-1/3 ">
+              <img src="/img/Profile.jpeg" alt="Perfil do Autor" className="w-full h-96 object-cover" />
+            </div>
+
+            <div className="text-white w-2/3 ml-12">
+              <h2 className="text-green-500 text-xl mb-2">Sobre mim</h2>
+              <h1 className="font-semibold text-2xl mb-2">Eu sou Victor Junqueira, sou desenvolvedor front-end...</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, eos accusamus dicta magnam cum a voluptas cumque, incidunt, at non perferendis iusto distinctio! Non sit amet vitae magnam quibusdam commodi?</p>
+              <div className="h-0.5 w-full bg-green-500 my-6"></div>
+
+              <div className="grid grid-cols-2 mb-6 gap-3">
+                <h3 className="text-white"><strong>Nome: </strong>Victor Junqueira</h3>
+                <h3 className="text-white"><strong>Idade: </strong>17 anos</h3>
+                <h3 className="text-white"><strong>Email: </strong>victorjunqueira.prog@gmail.com</h3>
+                <h3 className="text-white"><strong>Telefone: </strong>+55 (19) 99716-8299</h3>
+              </div>
+
+              <a href="#" className="px-4 py-2 color-white rounded-sm bg-green-500 text-black w-max text-lg">Download CV</a>
+
+            </div>
+          </div>
+        </section>
+        <section className="w-full bg-neutral-950 h-max">
+          <div className="mx-auto max-w-7xl flex flex-row py-12 text-white">
+            <div className="w-full h-full">
+              <h1 className="text-3xl font-semibold">Meus Serviços</h1>
+              <div className="grid grid-cols-3 gap-4 my-4">
+                <div className="bg-neutral-900 p-8 rounded-md shadow-sm shadow-neutral-700">
+                  <Globe className="text-green-500 w-12 h-12" />
+                  <h1 className="text-2xl font-semibold my-2">Desenvolvimento Web</h1>
+                  <p>Plataformas on-line, lojas virtuais e blogs.</p>
+                </div>
+                <div className="bg-neutral-900 p-8 rounded-md shadow-sm shadow-neutral-700">
+                  <Smartphone className="text-green-500 w-12 h-12" />
+                  <h1 className="text-2xl font-semibold my-2">Desenvolvimento Mobile</h1>
+                  <p>Aplicativos educacionais, plataformas de gerenciamento de conteúdo e entretenimento.</p>
+                </div>
+                <div className="bg-neutral-900 p-8 rounded-md shadow-sm shadow-neutral-700">
+                  <Database className="text-green-500 w-12 h-12" />
+                  <h1 className="text-2xl font-semibold my-2">Banco de Dados</h1>
+                  <p>Gerenciar e construir esquemas de bancos de dados relacionais e não relacionais.</p>
+                </div>
+                <div className="bg-neutral-900 p-8 rounded-md shadow-sm shadow-neutral-700">
+                  <Headphones className="text-green-500 w-12 h-12" />
+                  <h1 className="text-2xl font-semibold my-2">Suporte</h1>
+                  <p>Atendimento ao cliente dedicado e especializado.</p>
+                </div>
+                <div className="bg-neutral-900 p-8 rounded-md shadow-sm shadow-neutral-700">
+                  <File className="text-green-500 w-12 h-12" />
+                  <h1 className="text-2xl font-semibold my-2">Documentação e Artigos</h1>
+                  <p>Desenvolvimento de artigos de pesquisas e editais.</p>
+                </div>
+                <div className="bg-neutral-900 p-8 rounded-md shadow-sm shadow-neutral-700">
+                  <Coins className="text-green-500 w-12 h-12" />
+                  <h1 className="text-2xl font-semibold my-2">Vendas</h1>
+                  <p>Alavanque sua unidade de negócio com técnicas de vendas.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
