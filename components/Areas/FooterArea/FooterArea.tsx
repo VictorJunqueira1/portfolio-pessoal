b2c5e0e09d32c0a7b9292ec7dffcdb3988f07711
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-03-29 12:37:49 -0300

    feat: adicionando responsividade em FooterArea

--- components/Areas/FooterArea/FooterArea.tsx
+++ components/Areas/FooterArea/FooterArea.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 
 const FooterArea = () => {
   return (
-    <nav>
-      <footer className="bg-neutral-950 text-white py-6 flex-wrap">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap justify-between items-center">
-            <div className="text-lg">
-              <p>© 2024 Victor Junqueira. Todos os direitos reservados.</p>
-            </div>
-            <div className="text-lg">
-              <p>Contato: <a href="mailto:victorjunqueira.prog@gmail.com" className="text-white transition duration-500 hover:text-green-500">victorjunqueira.prog@gmail.com</a></p>
-            </div>
-            <div>
-              <a href="#" className="text-white transition duration-500 mx-2 hover:text-green-500">Página Inicial</a>
-              <a href="https://www.linkedin.com/in/victor-junqueira-789004281/" className="text-white transition duration-500 mx-2 hover:text-green-500">LinkedIn</a>
-              <a href="https://github.com/VictorJunqueira1" className="text-white transition duration-500 mx-2 hover:text-green-500">GitHub</a>
-            </div>
+    <footer className="bg-neutral-950 text-white py-6">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
+          <div className="text-lg text-center sm:text-left">
+            <p>© 2024 Victor Junqueira. Todos os direitos reservados.</p>
+          </div>
+          <div className="text-lg">
+            <p>Contato: <a href="mailto:victorjunqueira.prog@gmail.com" className="text-white transition duration-500 hover:text-green-500">victorjunqueira.prog@gmail.com</a></p>
+          </div>
+          <div className="flex justify-center">
+            <a href="#" className="text-white transition duration-500 mx-2 hover:text-green-500">Página Inicial</a>
+            <a href="https://www.linkedin.com/in/victor-junqueira-789004281/" className="text-white transition duration-500 mx-2 hover:text-green-500">LinkedIn</a>
+            <a href="https://github.com/VictorJunqueira1" className="text-white transition duration-500 mx-2 hover:text-green-500">GitHub</a>
           </div>
         </div>
-      </footer>
-    </nav>
+      </div>
+    </footer>
   );
 };
 
