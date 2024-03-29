d138261067bc6a78e5588b17d2d06ecbfa7d5146
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-03-29 14:24:21 -0300

    fix: troca do campo label senha para textarea.

--- components/Areas/ContactArea/ContactArea.tsx
+++ components/Areas/ContactArea/ContactArea.tsx
@@ -1,60 +1,60 @@
 import React from 'react';
 
 interface ContactAreaProps {
-  name: string;
-  setName: React.Dispatch<React.SetStateAction<string>>;
-  lastname: string;
-  setLastName: React.Dispatch<React.SetStateAction<string>>;
-  email: string;
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
-  password: string;
-  setPassword: React.Dispatch<React.SetStateAction<string>>;
-  error: string;
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    name: string;
+    setName: React.Dispatch<React.SetStateAction<string>>;
+    lastname: string;
+    setLastName: React.Dispatch<React.SetStateAction<string>>;
+    email: string;
+    setEmail: React.Dispatch<React.SetStateAction<string>>;
+    password: string;
+    setPassword: React.Dispatch<React.SetStateAction<string>>;
+    error: string;
+    handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
 const ContactArea: React.FC<ContactAreaProps> = ({
-  name,
-  setName,
-  lastname,
-  setLastName,
-  email,
-  setEmail,
-  password,
-  setPassword,
-  error,
-  handleSubmit
+    name,
+    setName,
+    lastname,
+    setLastName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    error,
+    handleSubmit
 }) => {
-  return (
-    <section id="contato" className="w-full bg-neutral-900 py-8 sm:py-12">
-      <div className="mx-auto max-w-4xl px-4 text-white">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl text-center font-semibold mb-8">Contato</h1>
-        <p className="mt-6 text-lg sm:text-xl text-center">Bem-vindo à seção de contato! <span className="block sm:inline mt-2 sm:mt-0">Se gostou do meu trabalho, é só me chamar!</span></p>
-        <div className="mt-10">
-          {error && <p className="text-center text-red-500">{error}</p>}
-          <form onSubmit={handleSubmit} className="flex flex-col items-center w-full">
-            <label className="w-full mb-6">
-              Nome:
-              <input type="text" placeholder="Insira seu nome" value={name} onChange={e => setName(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
-            </label>
-            <label className="w-full mb-6">
-              Sobrenome:
-              <input type="text" placeholder="Insira seu sobrenome" value={lastname} onChange={e => setLastName(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
-            </label>
-            <label className="w-full mb-6">
-              Email:
-              <input type="email" placeholder="Insira seu email" value={email} onChange={e => setEmail(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
-            </label>
-            <label className="w-full mb-6">
-              Senha:
-              <input type="password" placeholder="Insira sua senha" value={password} onChange={e => setPassword(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
-            </label>
-            <button type="submit" className="mt-6 w-36 h-12 bg-green-500 hover:bg-green-600 transition-colors text-lg font-semibold rounded-md shadow-md">Enviar</button>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section id="contato" className="w-full bg-neutral-900 py-8 sm:py-12">
+            <div className="mx-auto max-w-4xl px-4 text-white">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl text-center font-semibold mb-8">Contato</h1>
+                <p className="mt-6 text-lg sm:text-xl text-center">Bem-vindo à seção de contato! <span className="block sm:inline mt-2 sm:mt-0">Se gostou do meu trabalho, é só me chamar!</span></p>
+                <div className="mt-10">
+                    {error && <p className="text-center text-red-500">{error}</p>}
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center w-full">
+                        <label className="w-full mb-6">
+                            Nome:
+                            <input type="text" placeholder="Insira seu nome" value={name} onChange={e => setName(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
+                        </label>
+                        <label className="w-full mb-6">
+                            Sobrenome:
+                            <input type="text" placeholder="Insira seu sobrenome" value={lastname} onChange={e => setLastName(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
+                        </label>
+                        <label className="w-full mb-6">
+                            Email:
+                            <input type="email" placeholder="Insira seu email" value={email} onChange={e => setEmail(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-b border-gray-400 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" />
+                        </label>
+                        <label className="w-full mb-6">
+                            Mensagem:
+                            <textarea placeholder="Deixe sua mensagem" value={password} onChange={e => setPassword(e.target.value)} className="w-full p-2 mt-2 bg-transparent border-2 border-gray-500 hover:border-green-500 text-white focus:border-green-500 focus:outline-none transition-colors" rows="4"></textarea>
+                        </label>
+                        <button type="submit" className="mt-6 w-36 h-12 bg-green-500 hover:bg-green-600 transition-colors text-lg font-semibold rounded-md shadow-md">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default ContactArea;
